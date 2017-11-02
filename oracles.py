b74e9bbbfad5aac6626e69de247ffe15f43dabe5
tarun-t <thammisettytarun@gmail.com>
2017-11-02 12:24:54 +0530

    Naive oracle for picking docs from environment

--- oracles.py
+++ oracles.py
@@ -2,8 +2,11 @@ import logging
 import os
 import gensim
 import networkx as nx
+import numpy as np
 
 from gensim.models import LdaModel
+from scipy.stats import entropy as kld
+
 from lib.cnn_dmm_utils import chunk_list
 from train_topic_models import TrainTopicModel
 
@@ -13,48 +16,55 @@ logger.setLevel(logging.INFO)
 
 MODELS_DIR = 'models/'
 
-class TopicChains(object):
-    def __init__(self, dictionary, corpus, env, chunk_size=4000, model_name='lda'):
+class oracle(object):
+    def __init__(self, dictionary, corpus, env, passes=4, threshold=.9, num_topics=100, window=2, model='lda', model_name='lda'):
         self.dictionary = dictionary
         self.corpus = corpus
-        self.chunk_size = chunk_size
+        self.env = env
+        self.threshold = threshold
+        self.window = window
+        self.model = model
+        self.passes = passes 
         self.model_name = model_name
+        self.num_topics = num_topics
         self.models = []
-
-    def articles_from_env(self):
-        pass
-
-    def load_models(self):
-        try:
-            for model_no in range(len(self.corpus)/self.chunk_size):
-                mname = 'lda' + str(idx)
-                model = LdaModel.load(os.path.join(MODELS_DIR, mname))
-                self.models.append(model)
-            return
-        except:
-            self.models = []
-                
-    def train_models(self):
-        self.load_models()
-
-        if self.models:
-            return
-
-        corpus_len = len(self.corpus)
-        logger.info('No of docs in corpus {}'.format(corpus_len))
-        no_models = corpus_len/self.chunk_size
-        logger.info('No of LDA models {}'.format(no_models))
-        for idx, corpus in enumerate(chunk_list(self.corpus, self.chunk_size)):
-            logger.info('Corpus length {}'.format(len(corpus)))
-            model = TrainTopicModel(self.model_name, articles=[], corpus=corpus,
-                                    dictionary=self.dictionary, preprocess=False, num_topics=20)
-            mname = 'lda' + str(idx)
-            model = model.train_model(saved_model=mname, passes=20)
-            self.models.append(model)
-            model.save(os.path.join(MODELS_DIR, mname))
-            
-    def construct_topic_chains(self):
         self.train_models()
 
-    def imp_documents(self, input_docs):
-        pass
+    def train_models(self):
+        corpus_len = len(self.corpus)
+        logger.info('No of docs in corpus {}'.format(corpus_len))
+        model = TrainTopicModel(self.model, articles=[], corpus=self.corpus,
+                                dictionary=self.dictionary, preprocess=False, num_topics=self.num_topics)
+        model = model.train_model(passes=self.passes, saved_model=self.model_name)
+        self.models.append(model)
+        model.save(os.path.join(MODELS_DIR, self.model_name))
+
+    def get_active_topics(self, docs):
+        active_topics = set()
+        model = self.models[0]
+        if not docs:
+            return active_topics
+        for doc in docs:
+            topics = model.get_document_topics(doc[1], minimum_probability=.15)
+            for topic in topics:
+                active_topics |= set([topic[0]])
+        return active_topics
+
+    def imp_documents(self, docs, round_no):
+        model = self.models[0]
+        t_topics = [(doc[0], model.get_document_topics(doc[1], minimum_probability=.15)) for doc in docs]
+        if round_no <= self.window or np.random.uniform() > self.threshold:
+            logger.info("Resample docs")
+            return [docs[i] for i in range(len(docs))]
+        self.env.current_idx = docs[0][0]
+        prev_topics = set()
+        for day in range(1,self.window+1):
+            prev_topics |= self.get_active_topics(self.env.prev(n=day))
+        logger.info("Active Topics in the window {}".format(len(prev_topics)))
+        docs_chosen = []
+        for t_topic in t_topics:
+            topics = set([topic[0] for topic in t_topic[1]])
+            logger.info("Topics in set {}".format(len(topics)))
+            if bool(topics & prev_topics):
+                docs_chosen.append(t_topic[0])
+        return docs_chosen
