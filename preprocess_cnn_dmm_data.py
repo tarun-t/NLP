3b2a6558e7e9b1e45a9074d9b79b87539aab2018
tarun-t <thammisettytarun@gmail.com>
2017-11-02 12:19:25 +0530

    Refactor preprocessing utils

--- preprocess_cnn_dmm_data.py
+++ preprocess_cnn_dmm_data.py
@@ -1,10 +1,11 @@
 import os
+import re
 import logging
 import gensim
 
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
-from nltk.stem.wordnet import WordNetLemmatizer
+from gensim.utils import lemmatize
 from gensim.corpora import Dictionary
 from gensim.models import HdpModel, LdaModel
 from gensim.models import Phrases
@@ -37,31 +38,22 @@ def fetch_articles_from_data(data_path):
         if not len(art):
             logger.warning('Article length 0 for {}'.format(story))
             continue
-        articles.append(art.decode('utf-8'))
-
+        articles.append(art)
     return articles
 
 
 def tokenize_articles(articles):
     logger.info('tokenizing articles')     
-    tokenizer = RegexpTokenizer('\\w+')
+    texts = [[] for i in articles]
     for idx, art in enumerate(articles):
-        articles[idx] = tokenizer.tokenize(articles[idx])
-
-    return articles
-
+        texts[idx] = gensim.utils.simple_preprocess(' '.join(art), deacc=True, min_len=3)
+    return texts
 
 def add_phrases(articles, min_count=40):
+    print articles[0]
     logger.info('adding bigrams')
     bigram = Phrases(articles, min_count=min_count)
-    update_stopwords =set([])
-    for idx in range(len(articles)):
-        for token in bigram[articles[idx]]:
-            if '_' in token:
-                # Token is a bigram, add to document.
-                articles[idx].append(token)
-                update_stopwords = set(token.split('_')) | update_stopwords
-    articles = [[token for token in article if token not in update_stopwords] for article in articles]
+    articles = [bigram[line] for line in articles]
     return articles
 
 
@@ -76,25 +68,15 @@ def remove_stop_words(articles, stop_words=[]):
     from nltk.corpus import stopwords
     stopwords = set(stopwords.words('english'))
     stopwords = set(stopwords) | set(stop_words)
-    articles = [[token for token in article if len(token) > 2 and token not in stopwords and not token.isdigit()] for article in articles]
+    articles = [[token for token in article if token not in stopwords and not token.isdigit()] for article in articles]
     return articles
 
 
 def lemmatize_articles(articles):
     logger.info('lemmatizing articles')
-    lemmatizer = WordNetLemmatizer()
-    lemmatized_articles = []
-    for art in articles:
-        lemm_art = []
-        for token in art:
-            try:
-                lemm_art.append(lemmatizer.lemmatize(token))
-            except UnicodeError:
-                pass
-
-        lemmatized_articles.append(lemm_art)
-
-    return lemmatized_articles
+    print articles[0]
+    articles = [[word.split('/')[0] for word in lemmatize(' '.join(line), allowed_tags=re.compile('(NN)'), min_length=3)] for line in articles]
+    return articles
 
 def create_dict(articles):
     dictionary = Dictionary(articles)
@@ -109,16 +91,7 @@ def construct_dict(articles, dictionary_path=None):
         dictionary = gensim.corpora.Dictionary.load(dictionary_path)
     except:
         dictionary = create_dict(articles)
-
-def construct_dict(articles, dictionary_path=None, dict_name='cnn_dict.dict', create_new=False):
-    if create_new:
-        return create_dict(articles, dict_name)
-    try:
-        dictionary = corpora.Dictionary.load(dictionary_path)
-    except:
-        dictionary = create_dict(articles, dict_name)
     return dictionary
-
 
 def filter_extreme_values(dictionary):
     dictionary.filter_extremes(no_below=40)
@@ -139,18 +112,11 @@ def construct_corpus(articles, dictionary, corpus_path=None):
     return corpus
 
 def preprocess_articles(articles):
-    articles = tokenize_articles(articles)
-    stop_words = read_stop_words()
-    articles = remove_stop_words(articles, stop_words)
-    articles = lemmatize_articles(articles)
+    articles = list(tokenize_articles(articles))
+    print len(articles)
+    articles = remove_stop_words(articles, stop_words=[])
+    print len(articles)
     articles = add_phrases(articles)
+    articles = lemmatize_articles(articles)
+    print len(articles)
     return articles
-
-def construct_corpus(articles, dictionary, corpus_path=None, corpus_name='cnn_corpus.mm', create_new=False):
-    if create_new:
-        return create_corpus(articles, dictionary, corpus_name)
-    try:
-        corpus = gensim.corpora.mmcorpus.MmCorpus(corpus_path)
-    except:
-        corpus = create_corpus(articles, dictionary, corpus_name)
-    return corpus
